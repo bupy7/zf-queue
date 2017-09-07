1de786243eecaa0186eaaf40576dbef94cc341de
Vasily Belosludcev <bupy765@gmail.com>
2017-09-07 23:16:37 +0500

    more code

--- src/Service/TaskService.php
+++ src/Service/TaskService.php
@@ -6,9 +6,15 @@ use Bupy7\Queue\Entity\TaskInterface;
 use DateTime;
 use Bupy7\Queue\Manager\EntityManagerInterface;
 use Bupy7\Queue\Exception\InvalidValueException;
+use Zend\EventManager\EventManagerAwareInterface;
+use Zend\EventManager\EventManagerAwareTrait;
 
-class TaskService
+class TaskService implements EventManagerAwareInterface
 {
+    public const EVENT_BEFORE_SAVE = 'beforeSave';
+
+    use EventManagerAwareTrait;
+
     /**
      * @var EntityManagerInterface
      */
@@ -35,6 +41,8 @@ class TaskService
             ->setStatusId(TaskInterface::STATUS_WAIT)
             ->setCreatedAt(new DateTime)
             ->setNumberErrors(0);
+
+        $this->getEventManager()->trigger(self::EVENT_BEFORE_SAVE, $this, ['task' => $task]);
 
         $this->entityManager->persist($task);
         $this->entityManager->flush();
